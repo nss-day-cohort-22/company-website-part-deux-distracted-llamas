6542104733909553a1307d3f6c29f2b8b676b54b
Kimberly Bird <kimberly.j.bird@gmail.com>
2017-10-16 14:23:31 -0500

    css changes

---
+++ js/testimonialsController.js
@@ -0,0 +1,16 @@
+const packagesParse = JSON.parse(localStorage.getItem("packagesDatabase"))
+const packagesEl = document.getElementsByClassName("testimonials")[0]
+
+for (var i = 0; i < packagesParse.packageArray.length; i++) {
+        var item = packagesParse.packageArray[i];  
+        
+        packagesEl.innerHTML += `
+        <section class="card">
+        <img class="image" src=${item.image} width="500px">
+        <h3>Name: ${item.name}</h3>
+        <h3>From: ${item.from}</h3>
+        <section class="bottom-right"><h3>${item.package} Package</h3></section>
+        <section class="testimonialText"><h3><em>"${item.testimonial}"</em></h3></section>
+        </section>
+        `
+    }
--- js/testimonialsFactory.js
+++ js/testimonialsFactory.js
@@ -1,3 +1,4 @@
+// data
 const middleearthFact1 = {
     "name": "John, Jack, James, and Jeff",
     "from": "Austin, Texas",
@@ -10,7 +11,7 @@ const middleearthFact2 = {
     "name": "Sarah and Frank",
     "from": "New York City",
     "package": "Middle-Earth",
-    "testimonial": "It was Frank/'s dream to visit the sites of Middle-Earth so he was so happy that I surprised him with this trip as a birthday present! He was in heaven retracing Frodo/'s steps.",
+    "testimonial": "It was Frank's dream to visit the sites of Middle-Earth so he was so happy that I surprised him with this trip as a birthday present! He was in heaven retracing Frodo's steps.",
     "image": "../img/testimonials/middleearth2.jpg"
 }
 
@@ -18,7 +19,7 @@ const narniaFact1 = {
     "name": "Annie, Jane, and Michelle",
     "from": "San Diego, California",
     "package": "Narnia",
-    "testimonial": "We are all English majors and C.S. Lewis is one of our favorite authors. We knew that for spring break, we wanted to visit some of the sites from the movie, but we didn't know where to begin! Thank you to Jason for guiding us through this incredible tour!",
+    "testimonial": "We are all English majors and C.S. Lewis is one of our favorite authors. We knew that for spring break we wanted to visit some of the sites from the movie, but we didn't know where to begin! Thank you to Jason for guiding us through this incredible tour!",
     "image": "../img/testimonials/narnia1.jpg"
 }
 
@@ -26,7 +27,7 @@ const narniaFact2 = {
     "name": "LouEllen",
     "from": "London, UK",
     "package": "Narnia",
-    "testimonial": "I never thought that I'd have the chance to do a solo tour of the Narnia locations but after a lot of saving, I was able to come from the UK and make it happen!",
+    "testimonial": "I never thought that I'd have the chance to do a solo tour of the Narnia locations but after a lot of saving, I was able to come from the UK and make it happen! FairyBNB was affordable, easy to register, and they were great at communicating throughout the whole process.",
     "image": "../img/testimonials/narnia2.jpg"
 }
 
@@ -34,7 +35,7 @@ const hogwartsFact1 = {
     "name": "The Stevens Family",
     "from": "Brighton, UK",
     "package": "Hogwarts",
-    "testimonial": "We are the biggest Harry Potter fans out there (and Quidditch fans!) so we/'ve made visiting Hogwarts our annual family holiday trip. Each year, we make sure to book with Kimmy because she is so patient with our hyper kids.",
+    "testimonial": "We are the biggest Harry Potter fans out there (and Quidditch fans!) so we've made visiting Hogwarts our annual family holiday trip. Each year, we make sure to book with Kimmy because she is so patient with our hyper kids.",
     "image": "../img/testimonials/hogwarts1.jpg"
 }
 
@@ -42,7 +43,7 @@ const hogwartsFact2 = {
     "name": "The Petersen Family",
     "from": "Sydney, Australia",
     "package": "Hogwarts",
-    "testimonial": "James and I have read every Harry Potter book (and the fan fiction, of course!) and we had a blast drinking Butterbeer and visiting the shops of Hogsmeade.",
+    "testimonial": "James and I have read every Harry Potter book (and the fan fiction, of course!) and we had a blast drinking Butterbeer and visiting the shops of Hogsmeade. FairyBNB made it that much easier for us to make the most out of our trip.",
     "image": "../img/testimonials/hogwarts2.jpg"
 }
 
@@ -50,14 +51,15 @@ const westerosFact1 = {
     "name": "Miriam",
     "from": "Denmark",
     "package": "Westeros",
-    "testimonial": "Jenna made this visit to the real King/'s Landing and I am still in disbelief that I was so lucky to go! While walking through the city, I could see where some of the most famous scenes from Game of Thrones happened. I could practically hear King Joffrey and Sansa arguing in the background."
+    "testimonial": "Jenna was key in helping me visit the real King's Landing; I am still in disbelief that I was so lucky to go! While walking through the city, I could see where some of the most famous scenes from Game of Thrones happened. I could practically hear King Joffrey and Sansa arguing in the background.",
+    "image": "../img/testimonials/westeros1.jpg"
 }
 
 const westerosFact2 = {
     "name": "Shelly",
     "from": "Seattle, Washington",
     "package": "Westeros",
-    "testimonial": "Daenerys Targaryen is by far my favorite character on Game of Thrones. Jenna made this visit to Essos incredible - she had all sorts of background knowledge on the area and what it was like to film. Can't wait to visit King/'s Landing next year!",
+    "testimonial": "Daenerys Targaryen is by far my favorite character on Game of Thrones. Jenna made this visit to Essos incredible - she had all sorts of background knowledge on the area and what it was like to film. Can't wait to visit King's Landing next year!",
     "image": "../img/testimonials/westeros2.jpg"
 }
 
@@ -65,26 +67,30 @@ const westerosFact3 = {
     "name": "Landon",
     "from": "Miami, Florida",
     "package": "Westeros",
-    "testimonial": "I/'ve read all the Game of Thrones books, so it was crazy to visit Meereen in real life! I spent hours wandering through the maze of a city and my family is super jealous that I visited. We/'re ready to book our next trip with FairyBNB!",
+    "testimonial": "I've read all the Game of Thrones books, so it was crazy to visit Meereen in real life! I spent hours wandering through the maze of a city and my family is super jealous that I visited. We're ready to book our next trip with FairyBNB!",
     "image": "../img/testimonials/westeros3.jpg"
 }
 
+// array
 let package = []
 
-package.push(middleearthFact1)
+// pushing to array
 package.push(middleearthFact2)
-package.push(narniaFact1)
+package.push(middleearthFact1)
 package.push(narniaFact2)
-package.push(hogwartsFact1)
+package.push(narniaFact1)
 package.push(hogwartsFact2)
+package.push(hogwartsFact1)
 package.push(westerosFact1)
 package.push(westerosFact2)
 package.push(westerosFact3)
 
-let packagesFactory = {
-    "package": package
+// saving array
+let packagesDatabase = {
+    "packageArray": package
 }
 
-const packagesString = JSON.stringify(packagesFactory)
-localStorage.setItem("packagesFactory", packagesString)
+// JSON stringify
+const packagesString = JSON.stringify(packagesDatabase)
+localStorage.setItem("packagesDatabase", packagesString)
 
