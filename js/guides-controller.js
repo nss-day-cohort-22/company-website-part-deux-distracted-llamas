f54ed873858e41f53072e3c7966d8a94b661b974
Jenna Solis <jennabsol@gmail.com>
2017-10-16 15:42:57 -0500

    added files

---
+++ js/guides-controller.js
@@ -0,0 +1,23 @@
+// Grab history database from local stoage and place into object called guidesEntries
+const guidesEntries = JSON.parse(localStorage.getItem("guidesData"))
+
+// create placeholder for selected dom element
+const guidesElement = document.getElementsByClassName("gInfo")[0]
+
+//iterate over every key in the guides database and push into the DOM
+for (let i = 0; i < guidesEntries.length; i++) {
+    var element = guidesEntries[i];
+    
+    guidesElement.innerHTML += `
+    <article class="guides-entry">
+    <section class="content">
+    <p>${element.image}<p>
+    <div class="text">
+    <h2 class="name">${element.name}</h2>
+    <p class="age">Age: ${element.age}</p>
+    <p class="location">Location: ${element.location}</p>
+    <p class="content">${element.content}</p>
+    </div>
+    </section>
+    </article>`
+}
---
+++ js/guides-factory.js
@@ -0,0 +1,50 @@
+// Guides entry objects for database
+
+const kimmy = {
+    "name": "Kimmy Bird",
+    "age": "32",
+    "location": "Hogwarts",
+    "content": "Lorem ipsum dolor sit amet, quis proin luctus purus, lacus conubia. Morbi ac, vestibulum dui molestie a malesuada, congue eleifend molestie vestibulum quis, a a commodo amet fringilla, dolor at. In pede mi, magna nec arcu dapibus curabitur morbi. Dis amet varius magna pellentesque, id ante viverra quam massa eget, egestas faucibus voluptatem, suscipit vitae sapiente. Per luctus pede in id, vulputate sed nec vel, sapien fermentum vitae, tincidunt est mollis, non suspendisse ac nullam quisque. Ornare vel.",
+    "image": `<img src="img/guides/kimmy.jpg" alt="Guides" width="128" height="128">`
+}   
+
+const chase = {
+    "name": "Chase Steely",
+    "age": "34",
+    "location": "Middle-earth",
+    "content": "Lorem ipsum dolor sit amet, quis proin luctus purus, lacus conubia. Morbi ac, vestibulum dui molestie a malesuada, congue eleifend molestie vestibulum quis, a a commodo amet fringilla, dolor at. In pede mi, magna nec arcu dapibus curabitur morbi. Dis amet varius magna pellentesque, id ante viverra quam massa eget, egestas faucibus voluptatem, suscipit vitae sapiente. Per luctus pede in id, vulputate sed nec vel, sapien fermentum vitae, tincidunt est mollis, non suspendisse ac nullam quisque. Ornare vel.",
+    "image": `<img src="img/guides/jenna.jpg" alt="Guides" width="128" height="128">`
+}
+
+const jenna = {
+    "name": "Jenna Solis",
+    "age": "31",
+    "location": "Westeros",
+    "content": "Lorem ipsum dolor sit amet, quis proin luctus purus, lacus conubia. Morbi ac, vestibulum dui molestie a malesuada, congue eleifend molestie vestibulum quis, a a commodo amet fringilla, dolor at. In pede mi, magna nec arcu dapibus curabitur morbi. Dis amet varius magna pellentesque, id ante viverra quam massa eget, egestas faucibus voluptatem, suscipit vitae sapiente. Per luctus pede in id, vulputate sed nec vel, sapien fermentum vitae, tincidunt est mollis, non suspendisse ac nullam quisque. Ornare vel.",
+    "image": `<img src="/img/guides/chaseScuba.jpg" alt="Guides" width="128" height="128">`
+}
+
+const jason = {
+    "name": "Jason Figuroa",
+    "age": "34",
+    "location": "Narnia",
+    "content": "Lorem ipsum dolor sit amet, quis proin luctus purus, lacus conubia. Morbi ac, vestibulum dui molestie a malesuada, congue eleifend molestie vestibulum quis, a a commodo amet fringilla, dolor at. In pede mi, magna nec arcu dapibus curabitur morbi. Dis amet varius magna pellentesque, id ante viverra quam massa eget, egestas faucibus voluptatem, suscipit vitae sapiente. Per luctus pede in id, vulputate sed nec vel, sapien fermentum vitae, tincidunt est mollis, non suspendisse ac nullam quisque.",
+    "image": `<img src="img/guides/jason.jpg" alt="Guides" width="128" height="128">`
+}
+
+// Create guides object database
+
+const guidesData = []
+
+// push all entries into guides database
+
+guidesData.push(kimmy)
+guidesData.push(chase)
+guidesData.push(jenna)
+guidesData.push(jason)
+
+// place guidesData insto local storage
+
+const guidesDataString = JSON.stringify(guidesData)
+localStorage.setItem("guidesData", guidesDataString);
+
