e6ae7df2744774c3d055e0697cfd1689526a4697
Kimberly Bird <kimberly.j.bird@gmail.com>
2017-10-17 11:16:22 -0500

    commit with new changes

--- js/testimonialsController.js
+++ js/testimonialsController.js
@@ -5,13 +5,21 @@ for (var i = 0; i < packagesParse.packageArray.length; i++) {
         var item = packagesParse.packageArray[i];  
         
         packagesEl.innerHTML += `
-        <section class="card">
-        <img class="image" src=${item.image} width="500px">
-        <h3>Name: ${item.name}</h3>
-        <h3>From: ${item.from}</h3>
-        <section class="bottom-right"><h3>${item.package} Package</h3></section>
-        <section class="testimonialText"><h3><em>"${item.testimonial}"</em></h3></section>
-        <button class="button"><span>${item.button}</span></button>
-        </section>
+        <div class="card">   
+            <img class="image" src=${item.image} width="500px">
+            <div class="bottom-right">   
+                <h2>${item.package} Package</h2>
+            </div>
+            <div class="textMain">
+                <h3>${item.name}</h3>
+                <section>
+                    <h5>${item.from}</h5>
+                </section>
+                <section class="testimonialText">
+                    <h3><em>"${item.testimonial}"</em></h3>
+                </section>
+                <button class="button"><span>${item.button}</span></button>
+            </div>
+        </div>
         `
     }
--- js/testimonialsFactory.js
+++ js/testimonialsFactory.js
@@ -5,7 +5,7 @@ const middleearthFact1 = {
     "package": "Middle-Earth",
     "testimonial": "We had a blast visiting Hobbiton. We ate first and second lunch in a Hobbit's home! Chase was awesome and he knew all sorts of secret tidbits about the sites we visited.",
     "image": "../img/testimonials/middleearth1.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const middleearthFact2 = {
@@ -14,7 +14,7 @@ const middleearthFact2 = {
     "package": "Middle-Earth",
     "testimonial": "It was Frank's dream to visit the sites of Middle-Earth so he was so happy that I surprised him with this trip as a birthday present! He was in heaven retracing Frodo's steps.",
     "image": "../img/testimonials/middleearth2.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const narniaFact1 = {
@@ -23,7 +23,7 @@ const narniaFact1 = {
     "package": "Narnia",
     "testimonial": "We are all English majors and C.S. Lewis is one of our favorite authors. We knew that for spring break we wanted to visit some of the sites from the movie, but we didn't know where to begin! Thank you to Jason for guiding us through this incredible tour!",
     "image": "../img/testimonials/narnia1.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const narniaFact2 = {
@@ -32,7 +32,7 @@ const narniaFact2 = {
     "package": "Narnia",
     "testimonial": "I never thought that I'd have the chance to do a solo tour of the Narnia locations but after a lot of saving, I was able to come from the UK and make it happen! FairyBnB was affordable, easy to register, and they were great at communicating throughout the whole process.",
     "image": "../img/testimonials/narnia2.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const hogwartsFact1 = {
@@ -41,7 +41,7 @@ const hogwartsFact1 = {
     "package": "Hogwarts",
     "testimonial": "We are the biggest Harry Potter fans out there (and Quidditch fans!) so we've made visiting Hogwarts our annual family holiday trip. Each year, we make sure to book with Kimmy because she is so patient with our hyper kids.",
     "image": "../img/testimonials/hogwarts1.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const hogwartsFact2 = {
@@ -50,7 +50,7 @@ const hogwartsFact2 = {
     "package": "Hogwarts",
     "testimonial": "James and I have read every Harry Potter book (and the fan fiction, of course!) and we had a blast drinking Butterbeer and visiting the shops of Hogsmeade. FairyBnB made it that much easier for us to make the most out of our trip.",
     "image": "../img/testimonials/hogwarts2.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const westerosFact1 = {
@@ -59,7 +59,7 @@ const westerosFact1 = {
     "package": "Westeros",
     "testimonial": "Jenna was key in helping me visit the real King's Landing; I am still in disbelief that I was so lucky to go! While walking through the city, I could see where some of the most famous scenes from Game of Thrones happened. I could practically hear King Joffrey and Sansa arguing in the background.",
     "image": "../img/testimonials/westeros1.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const westerosFact2 = {
@@ -68,7 +68,7 @@ const westerosFact2 = {
     "package": "Westeros",
     "testimonial": "Daenerys Targaryen is by far my favorite character on Game of Thrones. Jenna made this visit to Essos incredible - she had all sorts of background knowledge on the area and what it was like to film. Can't wait to visit King's Landing next year!",
     "image": "../img/testimonials/westeros2.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 const westerosFact3 = {
@@ -77,7 +77,7 @@ const westerosFact3 = {
     "package": "Westeros",
     "testimonial": "I've read all the Game of Thrones books, so it was crazy to visit Meereen in real life! I spent hours wandering through the maze of a city and my family is super jealous that I visited. We're ready to book our next trip with FairyBnB!",
     "image": "../img/testimonials/westeros3.jpg",
-    "button": "View this Package"
+    "button": "View Packages"
 }
 
 // array
